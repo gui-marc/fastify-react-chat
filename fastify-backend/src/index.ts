c36ad87088448d5d779c7b27d29d1def8693785c
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2024-09-21 20:37:33 +0100

    chore: add host

--- fastify-backend/src/index.ts
+++ fastify-backend/src/index.ts
@@ -61,7 +61,7 @@ fastify.setErrorHandler((error, request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: PORT as number });
+    await fastify.listen({ port: PORT as number, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
